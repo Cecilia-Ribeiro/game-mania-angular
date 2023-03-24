0081be32acc3dcc232d6dafe2602bc02fafad0b2
Cecilia-Ribeiro <cecilia.ribeiro.414@gmail.com>
2023-03-24 15:51:15 -0300

    Commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, Component, HostListener, OnDestroy, OnInit } from "@angular/core";
+import { LoginService } from "./login.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Game Mania';
+  mostrarMenu:boolean = true;
+
+  constructor(private _loginService: LoginService){}
+
+  ngOnInit() {
+
+    this._loginService.getMostraMenu().subscribe(res => {
+      this.mostrarMenu = res;
+    })
+    
+  }
+
+  ngOnDestroy() {
+    localStorage.clear();
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
+import { AppComponent } from './app.componet';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { MenuProdutoComponent } from './restrito/menu-produto/menu-produto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    MenuComponent,
+    RodapeComponent,
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+    MenuProdutoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+    RestritoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +110,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Game Mania';
-}
+}*/
